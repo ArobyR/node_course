187a655dc9e86d756ab69eb5cb171dd97ecfea49
ArobyR <ar.rosario001@gmail.com>
2021-08-10 23:13:31 -0400

    first commit

---
+++ primera_app/index.js
@@ -0,0 +1,11 @@
+//import * as sys from "os";
+
+
+// utilizando ES5 y objetos globales
+require('./modulos/datos')
+
+// global.test = "Hi"
+
+// global.console.log(global.test)
+
+console.log(module)
