577f70bf46915dbb3a1de40bdf786f5da619157f
ArobyR <ar.rosario001@gmail.com>
2021-08-21 15:27:57 -0400

    add middlewares and routes

---
+++ API-Rest-MongoDB/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const usersRoute = require("./routes/users");
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use("/api/users", usersRoute);
+
+mongoose
+  .connect("mongodb://localhost:27017/test_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.log("Can't connected to mongoDB...", err));
+
+app.listen(port, () => {
+  console.log("API RestFul OK");
+});
