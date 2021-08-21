d67a6640ef2f7a07b85cfae8f2a66ca87c8e0f41
ArobyR <ar.rosario001@gmail.com>
2021-08-21 15:28:30 -0400

    add models

---
+++ API-Rest-MongoDB/models/courses_model.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const courseSchema = mongoose.Schema({
+
+})
+
+module.exports = mongoose.model("Course", courseSchema)
---
+++ API-Rest-MongoDB/models/users_model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  user_name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  user_state: {
+    type: Boolean,
+    default: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
