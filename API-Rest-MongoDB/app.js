cd351d280c8f2c391b64a2f4b41c1a0bb6e32626
ArobyR <ar.rosario001@gmail.com>
2021-08-21 22:54:45 -0400

    add course route

--- API-Rest-MongoDB/app.js
+++ API-Rest-MongoDB/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const usersRoute = require("./routes/users");
+const courseRoute = require("./routes/courses")
 
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use("/api/users", usersRoute);
+app.use("/api/courses", courseRoute)
 
 mongoose
   .connect("mongodb://localhost:27017/test_db", {
