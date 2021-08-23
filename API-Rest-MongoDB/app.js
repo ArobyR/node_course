5f97c064f6ab253727b26b23d7b49553ad19e1ba
ArobyR <ar.rosario001@gmail.com>
2021-08-23 11:50:07 -0400

    update syntax

--- API-Rest-MongoDB/app.js
+++ API-Rest-MongoDB/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const usersRoute = require("./routes/users");
-const courseRoute = require("./routes/courses")
+const courseRoute = require("./routes/courses");
 
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", usersRoute);
-app.use("/api/courses", courseRoute)
+app.use("/api/courses", courseRoute);
 
 mongoose
   .connect("mongodb://localhost:27017/test_db", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    autoIndex: true,
   })
   .then(() => console.log("Connected to mongodb..."))
   .catch((err) => console.log("Can't connected to mongoDB...", err));
--- API-Rest-MongoDB/helpers/validateUser.js
+++ API-Rest-MongoDB/helpers/validateUser.js
@@ -4,7 +4,7 @@ function validateUser(user_name, password, email) {
   const schema = Joi.object({
     user_name: Joi.string().min(3).max(30).required(),
 
-    password: Joi.string().min(6).max(30).pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+    password: Joi.string().min(3).pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
 
     email: Joi.string().email({
       minDomainSegments: 2,
@@ -18,7 +18,7 @@ function validateUser(user_name, password, email) {
       password: password,
     });
   }
-
+  
   return schema.validate({
       user_name: user_name,
       email: email,
