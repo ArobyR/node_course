983593d8cb1efeff8446fed95dad6322c5c3a08c
ArobyR <ar.rosario001@gmail.com>
2021-08-23 11:46:48 -0400

    Protecting user

--- API-Rest-MongoDB/services/UserService.js
+++ API-Rest-MongoDB/services/UserService.js
@@ -2,15 +2,16 @@ const User = require("../models/users_model");
 
 async function createUser(body) {
   const result = new User({
-    email: body.email,
     user_name: body.user_name,
+    email: body.email,
     password: body.password,
   });
   return await result.save();
 }
 
 async function getUser() {
-  const users = await User.find({ user_state: true });
+  const users = await User.find({ user_state: true })
+  .select({user_name: 1, email: 1});
   return users;
 }
 
